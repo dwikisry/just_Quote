ec54640b1c0b0838fef6f4056dc2c97574d8a2b3
dwikisry <suryadwiki4@mhs.mdp.ac.id>
2022-12-08 13:01:10 +0700

    Update AdapterQuote.java
    08/12/2022

--- app/src/main/java/com/ahmfarisi/justquote/Adapter/AdapterQuote.java
+++ app/src/main/java/com/ahmfarisi/justquote/Adapter/AdapterQuote.java
@@ -1,14 +1,48 @@
 package com.ahmfarisi.justquote.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ahmfarisi.justquote.Model.QuoteModel;
 import com.ahmfarisi.justquote.R;
 
-public class AdapterQuote {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterQuote extends RecyclerView.Adapter<AdapterQuote.ViewHolder>{
+    private List<QuoteModel> listquote = new ArrayList<>();
+    private Context ctx;
+
+    public AdapterQuote(List<QuoteModel> listquote, Context ctx) {
+        this.listquote = listquote;
+        this.ctx = ctx;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View V = LayoutInflater.from(ctx).inflate(R.layout.item_quote, parent, false);
+        return new ViewHolder(V);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        QuoteModel QM = listquote.get(position);
+        holder.tvText.setText(QM.getText());
+        holder.tvAuthor.setText(QM.getAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listquote.size();
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvText, tvAuthor;
 
@@ -16,7 +50,7 @@ public class AdapterQuote {
             super(itemView);
             tvText = itemView.findViewById(R.id.tv_text);
             tvAuthor = itemView.findViewById(R.id.tv_author);
-            
+
         }
     }
 }
